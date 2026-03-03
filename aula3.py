37f46d3eb973c35ff0bdfec8275819991f982be3
unknown <20241pvai0030012@estudantes.ifpr.edu.br>
2026-03-03 09:59:47 -0300

    Olha o Romarinhooooooooooo

---
+++ aula3.py
@@ -0,0 +1,22 @@
+import random
+
+
+# while():
+#     digito = int(input("digite um numero de 17 a 22: "))
+#     valor = random.randint(17, 22) 
+
+#     if(digito < 22 and digito > 17):
+#         print(valor)
+#         if(digito == valor):
+#             print("acertou fiadaputa")
+#             break
+#         elif(digito != valor):
+#             print("burro p kraiKKKKKKKKKKKKKKKKKKKKKK")    
+
+for i in range (1,11): 
+    print(i)
+
+
+numeros = [0] * 10
+numeros[8] = 22
+print(numeros)
